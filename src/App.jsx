659cc952bd81df2103fbcbebaaf1bc892a218a01
sadeeabdullah <sadeeabdullah016@gmail.com>
2024-05-28 17:43:21 +0600

    adding navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from "@material-tailwind/react";
 
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <Button>Button</Button>
+      <div className='thediv'>hello after meeting the people of the heaven</div>
     </>
   )
 }
