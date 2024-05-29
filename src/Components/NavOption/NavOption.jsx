1ec644429a798686cb9bfd4640906318ddfe182c
sadeeabdullah <sadeeabdullah016@gmail.com>
2024-05-29 12:58:07 +0600

    adding scroll effect on the navbar

--- src/Components/NavOption/NavOption.jsx
+++ src/Components/NavOption/NavOption.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
+import { Link } from 'react-scroll'
 
 const NavOption = () => {
   return (
     <div >
         <ul className=' text-lg text-textColor gap-8 hidden lg:flex'>
-            <li>Home</li>
-            <li>About</li>
-            <li>Project</li>
-            <li>Contacts</li>
+            <Link
+              to="home"
+              spy={true} 
+              hashSpy={true}
+              isDynamic={true}
+              ignoreCancelEvents={true}
+              spyThrottle={500}
+              className='cursor-pointer'
+              
+              smooth={true} 
+              offset={50} 
+              duration={500} >
+              Home
+            </Link>
         </ul>
     </div>
   )
 }
 
 export default NavOption
+
+
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Drawer from '../Drawer/Drawer.jsx'
 import Logo from '../../assets/images/logo.png'
 import NavOption from '../NavOption/NavOption.jsx'
 
 const Navbar = () => {
+  const [scroll,setScroll] = useState(false);
+  const handleScroll = () => {
+    if(window.scrollY>50){
+      setScroll(true);
+    }else{
+      setScroll(false)
+    }
+  }
   return (
     <div>
       <div className='flex justify-between items-center lg:max-w-7xl lg:mx-auto py-4 mx-4 '>
