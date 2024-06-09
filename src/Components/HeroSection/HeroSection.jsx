eb4905286ff1efcfdc08e8e09352b3d69ae9c0de
sadeeabdullah <sadeeabdullah016@gmail.com>
2024-06-09 18:15:16 +0600

    initializing hero section

---
+++ src/Components/HeroSection/HeroSection.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <div>
+
+        {/* div for the social media */}
+        <div>
+
+        </div>
+
+        {/* div for hero text */}
+        <div>
+            
+        </div>
+        <div></div>
+    </div>
+  )
+}
+
+export default HeroSection
