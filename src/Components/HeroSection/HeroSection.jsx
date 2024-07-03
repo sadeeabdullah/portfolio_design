4b4cffe78fa280954b9d769db3928dc485f7a5b1
sadeeabdullah <sadeeabdullah016@gmail.com>
2024-07-03 20:44:33 +0600

    adding animation for fun

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -5,10 +5,10 @@ import bannerIMage from '../../assets/images/developer.png'
 
 const HeroSection = () => {
   return (
-    <div>
+    <div className='flex justify-between mt-12'>
       <SocialMedia/>
-        <div className='h-[100px] w-{'>
-            <img className='w-full h-full' src={bannerIMage} alt="" />
+        <div className='h-[400px] w-[400px]'>
+            <img className='w-full h-full rounded-full shadow-custom-shadow animate-pulse' src={bannerIMage} alt="" />
         </div>
         <div></div>
     </div>
